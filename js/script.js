2d43f28c144ef3705c0b8e811a0f32df6c9f6902
rebecca williams <twinedthorns@gmail.com>
2016-11-15 10:55:12 -0600

    getRandomQuote no longer prints repeated quotes, however current method works poorly
    Current getRandomQuote function does not print anything once all five
    quotes have been printed due to use of slice mutating the original
    quotes array.

--- js/script.js
+++ js/script.js
@@ -64,23 +64,36 @@ var randColor = function(carray){
 };
 
 
+//takes current list of quotes
+//first loop selects a quote that is == rand
+  //removes quote object, stores in new array
+  //returns removed quote
+//when loop runs again, array has been updated and contains one less item
+//repeat
+//when qarray.length = 0, start process over using new array
+
+
 var getRandomQuote = function(qarray) {
-  var rand = Math.floor(Math.random() * qarray.length); //chooses number between 0 and amt of quotes
+  var used = [];
   for (var i = 0; i < qarray.length; i++) {
-    if (rand === i) {
-      return qarray[i]; //returns quote object in its entirety
-    }
+    used = qarray.splice(Math.floor(Math.random()*qarray.length),1)[i];
+    return used;
   }
 };
+
+//used = qarray.splice(Math.random() * qarray.length, 1)[i];
+
+
+
 
 
 //this looks like a wreck
 var printQuote = function() {
-
-  var selectedQuote = getRandomQuote(quotes);
+	var formattedQuote;
   var color = randColor(colors);
-
-  if (selectedQuote.year && selectedQuote.citation === " ") {
+  var selectedQuote = getRandomQuote(quotes);
+  
+	if (selectedQuote.year && selectedQuote.citation === " ") {
     formattedQuote = "<p class='quote'>" + selectedQuote.quote + "</p>" +
       "<p class='source'>" + selectedQuote.source + "</p>";
   } //if both year and citation are missing
@@ -100,8 +113,7 @@ var printQuote = function() {
       "<span class='citation'>" + selectedQuote.citation + "</span>" +
       "<span class='year'>" + selectedQuote.year + "</span>" +
       "</p>";
-  }
-
+    }
   document.body.style.backgroundColor = color;
   document.getElementById('quote-box').innerHTML = formattedQuote;
 };
@@ -109,22 +121,10 @@ var printQuote = function() {
 
 printQuote();
 
+// window.setInterval(printQuote, 3000); //For example, every 30 seconds, make a new quote appear. 
+
+
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
-
-
-//Don't display a random quote more than once until ALL quotes from the array have been displayed. 
-//To help reviewers (and yourself) verify that the quotes don’t repeat until they’ve all been displayed, 
-//log the quote to the console each time the “Show Another Quote” button is clicked.
-  //need to know length of array, when end is reached, that's how we know all have been used
-  //somehow identify each quote and compare against upcoming quote/new random number
-     //quotes could be 'identified' just by their position in the initial array
-  //make array to hold used quotes? feels like overkill
-  //when the rand number is generated, note the index position of array/quote that it matches 
-
-
-//Refresh the quote after a set amount of time. 
-//For example, every 30 seconds, make a new quote appear. 
-//(You can use the setInterval() or setTimeout() method to do this
